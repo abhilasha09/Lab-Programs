dffa33ba8b1b6688796805b43d8f4d932fcd80aa
abhilasha09 <46891660+abhilasha09@users.noreply.github.com>
2019-03-10 18:26:28 +0530

    Create diagonal*.c

---
+++ diagonal*.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+int input(int m,int n,int a[m][n])
+{
+    
+    printf("Enter the elements");
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+        printf("element in row%d column%d",i+1,j+1);
+        scanf("%d",&a[i][j]);
+        }
+    }
+}
+int compute(int m,int n,int a[m][n],int b[])
+{
+  int f=0;
+  for(int c = 0; c < m; c++)
+  {
+        for(int d = 0; d < n; d++) 
+            {
+                if(c==d)
+                {
+                    int i=a[c][d];
+                    int b[m];
+                    b[f]=i;
+                    ++f;
+                }
+                
+            }
+    }
+  return f;  
+}
+int display(int m,int n,int f,int b[])
+{
+    if(m==n)
+    {
+        printf("The diagonal elements are\n");
+        for(int i=0;i<f;i++)
+        printf("%d",b[i]);
+        printf("\n");
+    }
+    else
+    printf("A rectangular matrix can't have diagonal");
+}
+void main()
+{
+    int m,n;
+    printf("Enter order");
+    scanf("%d%d",&m,&n);
+    int a[m][n];
+    input(m,n,a);
+    int b[m];
+    int f=compute(m,n,a,b);
+    display(m,n,f,b);
+}
