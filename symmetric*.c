168cda55ccbf35a2e8afdbf05c62c6190656066a
abhilasha09 <46891660+abhilasha09@users.noreply.github.com>
2019-03-10 12:55:59 +0530

    Update and rename symmetric.c to symmetric*.c

---
+++ symmetric*.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+int input(int m,int n,int a[m][n])
+{
+    
+    printf("Enter the elements");
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+        printf("element in row%d column%d",i+1,j+1);
+        scanf("%d",&a[i][j]);
+        }
+    }
+}
+int compute(int m,int n,int a[m][n],int b[n][m])
+{
+  for(int c = 0; c < m; c++) // to iterate the rows
+        for(int d = 0; d < n; d++) //to iterate the columns
+            b[d][c] = a[c][d];
+}
+int display(int m,int n,int a[m][n],int b[n][m])
+{
+    printf("The matrix\n");
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("The transpose matrix\n");
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            printf("%d\t",b[i][j]);
+        }
+        printf("\n");
+    }
+}
+int output(int m,int n,int a[m][n],int b[n][m])
+{
+    int flag;flag=0;
+    if(m==n)
+    {
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(a[i][j]!=b[i][j])
+                {
+                    flag=0;
+                    break;
+                }
+            }
+            
+        }
+        if(flag==0)
+            {
+                printf("\nThe matrix is not symmetric");
+            }
+        if(flag==1)
+        {
+            printf("\nThe matrix is symmetric");
+        }
+    }
+    else
+    {
+        printf("\nThe matrix is not symmetric");
+    }
+}
+void main()
+{
+    int m,n,flag;
+    printf("Enter the order of the square matrix");
+    scanf("%d%d",&m,&n);
+    int a[m][n];int b[n][m];
+    input(m,n,a);
+    compute(m,n,a,b);
+    display(m,n,a,b);
+    output(m,n,a,b);
+}
