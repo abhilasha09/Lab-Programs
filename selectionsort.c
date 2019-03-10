e81257dcbf6a8cea5f832105dad0f89252776766
abhilasha09 <46891660+abhilasha09@users.noreply.github.com>
2019-03-10 17:33:30 +0530

    Create selectionsort.c

---
+++ selectionsort.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int input(int n,int a[])
+{
+    printf("Enter the elements");
+    for(int i=0;i<n;i++)
+    {
+                    printf("Enter the element %d",i+1);
+                    scanf("%d",&a[i]);
+}
+}
+void swap(int *x, int *y) 
+{ 
+    int temp = *x;
+    *x= *y; 
+    *y= temp; 
+} 
+int sort(int n,int a[])
+{
+    int i,j,min;
+    for (i = 0; i < n-1; i++) 
+    { 
+        min= i; 
+        for(j=i+1;j<n;j++)
+          if (a[j]<a[min]) 
+            min=j; 
+     swap(&a[min],&a[i]); 
+    } 
+} 
+int output(int n,int a[])
+{
+    printf("The minimum number=%d",a[0]);
+    printf("The maximum number=%d",a[n-1]);
+}
+            
+int main()
+{
+    int n;
+    printf("Enter n");
+    scanf("%d",&n);
+    int a[n];
+    input(n,a);
+    sort(n,a);
+    output(n,a);
+}
