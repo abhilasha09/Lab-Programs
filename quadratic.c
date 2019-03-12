b040b2cc03f583406c0d1e1d8783fba8dd284176
abhilasha09 <46891660+abhilasha09@users.noreply.github.com>
2019-03-12 17:29:38 +0530

    Update quadratic.c

--- quadratic.c
+++ quadratic.c
@@ -5,22 +5,25 @@ struct complex
 	float real;
 	float imaginary;
 };
-void input(float *a,float *b,float *c)
+void coefficients(float *a,float *b,float *c)
 {
 	printf("Enter the coefficients a,b,c");
 	scanf("%f%f%f",a,b,c);
 }
-void compute(float a,float b,float c,float *r1,float *r2,float *det,struct complex *m)
+void get_roots(float a,float b,float c,float *det,struct complex *m,struct complex *m1)
 {
 	*det=b*b-4*a*c;
 	if(*det>0)
 	{
-	 	*r1=(-b+sqrt(*det))/(2*a);
- 		*r2=(-b-sqrt(*det))/(2*a);
+	 	m->real=(-b+sqrt(*det))/(2*a);
+		m->imaginary=0;
+ 		m1->real=(-b-sqrt(*det))/(2*a);
+		m1->imaginary=0;
 	}
 	if(*det==0)
 	{
-		*r1=*r2=-b/(2*a);
+		m->real=m1->real=-b/(2*a);
+		m->imaginary=m1->imaginary=0;
 	}
 	if(*det<0)
 	{
@@ -28,22 +31,23 @@ void compute(float a,float b,float c,float *r1,float *r2,float *det,struct compl
 		m->imaginary=sqrt(-(*det)/(2*a));//imaginary part
 	}
 }
-void output(float r1,float r2,float det,struct complex m)
+void display(float det,struct complex m,struct complex m1)
 {
 	if(det>0)
-	printf("Roots are real and unequal: %f and %f",r1,r2);
+	printf("Roots are real and unequal: %f+i %f and %f+i %f",m.real,m.imaginary,m1.real,m1.imaginary);
 	if(det<0)
-	printf("Roots are imaginary: %f +i %f,%f-i%f",m.real,m.imaginary,m.real,m.imaginary);
+	printf("Roots are imaginary: %f +i %f,%f-i%f",m.real,m.imaginary,m1.real,m1.imaginary);
 	if(det==0)
-	printf("Roots are real and equal: %f and %f",r1,r2);
+	printf("Roots are real and equal: %f+i %f and %f+i %f",m.real,m.imaginary,m.real,m1.imaginary);
 }
 int main()
 {
 	float a,b,c,r1,r2,det;
-	input(&a,&b,&c);
+	coefficients(&a,&b,&c);
 	struct complex m;
-	compute(a,b,c,&r1,&r2,&det,&m);
-	output(r1,r2,det,m);
+	struct complex m1;
+	get_roots(a,b,c,&det,&m,&m1);
+	display(det,m,m1);
 	return 0;
 }
 
