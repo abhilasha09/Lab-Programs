27245426a54cb7faee55a4e277f1e49f5bccbf12
abhilasha09 <46891660+abhilasha09@users.noreply.github.com>
2019-03-12 17:48:11 +0530

    Update nCrnPr.c

--- nCrnPr.c
+++ nCrnPr.c
@@ -1,27 +1,19 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
-void input(int *n,int *r)
+void get_n_r(int *n,int *r)
 {
     printf("Enter the n of nCr and nPr");
     scanf("%d",n);
     printf("Enter the r of nCr and nPr");
     scanf("%d",r);
 }
-int fact(int n)
+int factorial(int n)
 {
     int f;f=1;
     for(int i=1;i<=n;i++)
         f=f*i;
         return f;
 }
-int compute(int n,int r,int *npr,int *ncr)
+int compute_ncr_npr(int n,int r,int *npr,int *ncr)
 {
     int n1,r1,n_r,flag;flag=1;int s=n-r;
     if(n>=r)
@@ -36,7 +28,7 @@ int compute(int n,int r,int *npr,int *ncr)
     flag=0;
     return flag;
 }
-void output(int ncr,int npr,int flag)
+void display(int ncr,int npr,int flag)
 {
     if(flag==0)
     printf("sorry n>r so nCr and nPr is not possible");
@@ -46,9 +38,9 @@ void output(int ncr,int npr,int flag)
 void main()
 {
     int n,r,ncr,npr,flag,f;
-    input(&n,&r);
-    f=fact(n);
-    flag=compute(n,r,&npr,&ncr);
-    output(ncr,npr,flag);
+    get_n_r(&n,&r);
+    f=factorial(n);
+    flag=compute_ncr_npr(n,r,&npr,&ncr);
+    display(ncr,npr,flag);
     
 }
